3133e76437e8ab4c52ee826a56e6b98758c4abb3
sestoculo <tonci@protonmail.com>
2018-03-20 11:47:34 +0100

    Functional test now checks we can input a to-do item

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 #Tests are organised into classes, which inherit from unittest.TestCase.
@@ -30,7 +31,32 @@ class NewVisitorTest(unittest.TestCase):
         #assertEqual, assertTrue, assertFalse, and so on. You can find more in the
         #unittest documentation.
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
         #self.fail just fails no matter what, producing the error message given. I’m
         #using it as a reminder to finish the test.
         self.fail('Finish the test')
@@ -47,13 +73,13 @@ if __name__ == '__main__': #
     unittest.main(warnings='ignore')
 
 
-# She is invited to enter a to-do item straight away
-# She types "Buy peacock feathers" into a text box (Edith's hobby
-# is tying fly-fishing lures)
-# When she hits enter, the page updates, and now the page lists
-# "1: Buy peacock feathers" as an item in a to-do list
-# There is still a text box inviting her to add another item. She
-# enters "Use peacock feathers to make a fly" (Edith is very methodical)
+#We’re using several of the methods that Selenium provides to examine web pages:
+#find_element_by_tag_name, find_element_by_id, and find_elements_by_tag_name
+#(notice the extra s, which means it will return several elements rather than just one).
+#We also use send_keys, which is Selenium’s way of typing into input elements. You’ll
+#also see the Keys class (don’t forget to import it), which lets us send special keys like
+#Enter, but also modifiers like Ctrl.
+
 # The page updates again, and now shows both items on her list
 # Edith wonders whether the site will remember her list. Then she sees
 # that the site has generated a unique URL for her -- there is some
